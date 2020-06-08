8255d6799902ebf4854a14a198c58d61b72e3068
konakona <54853315@qq.com>
2020-06-08 18:10:39 +0800

    图片模型

---
+++ models/image.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"FoodBackend/pkg/api/dto"
+	"FoodBackend/pkg/e"
+	"FoodBackend/pkg/util"
+)
+
+type Image struct {
+	Model
+	CreateUserId int    `json:"create_user_id"`
+	Url          string `json:"url"`
+}
+
+func (Image) Create(dto dto.UploadDto) (Image, int) {
+	image := Image{
+		Url:          dto.File,
+		CreateUserId: dto.CreateUserId,
+	}
+	util.Log.Notice("ImageModel:", image)
+	result := db.Create(&image)
+	if result.Error == nil {
+		return image, 0
+	}
+	util.Log.Error(result.Error.Error())
+	return image, e.ERROR
+}
+
+func (Image) Delete(image *Image) bool {
+	//db.Delete(&Image{}, "id = ?", id)
+	if db.Delete(image).GetErrors() == nil {
+		return true
+	}
+	return false
+}
